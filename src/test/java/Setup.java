ae34b5ff0c04e6140733beac559007dba3c61ec8
ibrahim <isharif@famcare.app>
2023-02-12 18:43:03 +0200

    set chrome driver to headless

--- src/test/java/Setup.java
+++ src/test/java/Setup.java
@@ -1,5 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -9,10 +10,12 @@ public class Setup {
     public static WebDriverWait wait;
 @Test
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 60);
-        driver.get("https://famcare.app");
+    ChromeOptions options= new ChromeOptions();
+    options.addArguments("--headless");
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver(options);
+    wait = new WebDriverWait(driver, 60);
+    driver.get("https://famcare.app");
     }
 
 }
